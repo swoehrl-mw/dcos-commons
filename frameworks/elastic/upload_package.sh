11932b66535b6039666dc5f5bd518714d6321ddd
Sebastian Woehrl <sebastian.woehrl@maibornwolff.de>
2018-11-04 11:13:01 +0100

    Update to 6.4.2

--- frameworks/elastic/upload_package.sh
+++ frameworks/elastic/upload_package.sh
@@ -1,2 +1,2 @@
-VERSION=2.4.0-6.3.1-mw2
-S3_BUCKET=sebastianw-dcos-dev S3_DIR_PATH=packages S3_DIR_NAME=$VERSION ./build.sh aws $VERSION
+VERSION=2.4.0-6.4.2-mw1
+S3_BUCKET=mw-universe S3_DIR_PATH=packages S3_DIR_NAME=$VERSION ./build.sh aws $VERSION
